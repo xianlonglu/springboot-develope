470a07a891457d0ed79071972b61af60d1425ad9
rookies <408030514@qq.com>
2019-01-17 18:48:15 +0800

    modefy mail

--- springboot-mail/src/main/java/mail/service/MailSendService.java
+++ springboot-mail/src/main/java/mail/service/MailSendService.java
@@ -22,61 +22,98 @@ import org.springframework.stereotype.Service;
  *
  */
 @Service
-public class MailSendService
-{
+public class MailSendService {
 	@Autowired
 	private JavaMailSender mailSender;
-	
-	/**日志打印.*/
+
+	/** 日志打印. */
 	private Logger logger = Logger.getLogger(MailSendService.class);
-	
+
 	/**
-	 * 发送简单邮件
-	 * @param fromPos 发送地址
-	 * @param toPos 目的地址
-	 * @param subject 邮件主题
-	 * @param text 邮件内容
+	 * 发送静态资源的文件,如：图片
+	 * 
+	 * @param fromPos
+	 *            发送地址
+	 * @param toPos
+	 *            目的地址
+	 * @param subject
+	 *            邮件主题
+	 * @param text
+	 *            邮件内容
 	 * @return 发送是否成功
 	 */
-	public boolean sendSimpleMail(String fromPos, String toPos, String subject, String text)
-	{
+	public boolean sendHtmlMail(String fromPos, String toPos, String subject,
+			String text) {
+		MimeMessage msg = mailSender.createMimeMessage();
+		try {
+			// MimeMessageHelper构造器，如果要发送附件邮件，必须指定multipart参数为true
+			MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+			helper.setFrom(fromPos);
+			helper.setTo(toPos);
+			helper.setSubject(subject);
+			helper.setText(text,true);
+			mailSender.send(msg);
+		} catch (Exception e) {
+			logger.error("发送附件邮件失败.", e);
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * 发送简单邮件
+	 * 
+	 * @param fromPos
+	 *            发送地址
+	 * @param toPos
+	 *            目的地址
+	 * @param subject
+	 *            邮件主题
+	 * @param text
+	 *            邮件内容
+	 * @return 发送是否成功
+	 */
+	public boolean sendSimpleMail(String fromPos, String toPos, String subject,
+			String text) {
 		SimpleMailMessage msg = new SimpleMailMessage();
 		msg.setFrom(fromPos);
-		//可添加多个目的地址
+		// 可添加多个目的地址
 		msg.setTo(toPos);
 		msg.setSubject(subject);
 		msg.setText(text);
-		//msg.setBcc(bcc) 密送地址 
-		//msg.setCc(cc) 抄送地址
-		try
-		{
+		// msg.setBcc(bcc) 密送地址
+		// msg.setCc(cc) 抄送地址
+		try {
 			mailSender.send(msg);
-		}
-		catch (Exception e) 
-		{
+		} catch (Exception e) {
 			logger.error("发送简单邮件失败.", e);
 			return false;
 		}
 		return true;
 	}
-	
+
 	/**
 	 * 发送带有附件的邮件
-	 * @param fromPos 发送地址
-	 * @param toPos 目的地址
-	 * @param subject 邮件主题
-	 * @param file 附件
-	 * @return 发送是否成功 
+	 * 
+	 * @param fromPos
+	 *            发送地址
+	 * @param toPos
+	 *            目的地址
+	 * @param subject
+	 *            邮件主题
+	 * @param file
+	 *            附件
+	 * @return 发送是否成功
 	 */
-	public boolean sendAttachFileMail(String fromPos, String toPos, String subject, String text, FileSystemResource file)
-	{
+	public boolean sendAttachFileMail(String fromPos, String toPos,
+			String subject, String text, FileSystemResource file) {
 		MimeMessage msg = mailSender.createMimeMessage();
-		try
-		{
-			//MimeMessageHelper构造器，如果要发送附件邮件，必须指定multipart参数为true
-			MimeMessageHelper helper  = new MimeMessageHelper(msg, true);
+		try {
+			// MimeMessageHelper构造器，如果要发送附件邮件，必须指定multipart参数为true
+			MimeMessageHelper helper = new MimeMessageHelper(msg, true);
 			try {
-				helper.setFrom(new InternetAddress(fromPos,"鲁贤龙邮箱","utf-8"));
+				helper.setFrom(new InternetAddress(fromPos, "鲁贤龙邮箱", "utf-8"));
 			} catch (UnsupportedEncodingException e) {
 				e.printStackTrace();
 			}
@@ -85,46 +122,47 @@ public class MailSendService
 			helper.setText(text);
 			helper.addAttachment(file.getFilename(), file);
 			mailSender.send(msg);
-		} 
-		catch (MessagingException e)
-		{
+		} catch (MessagingException e) {
 			logger.error("发送附件邮件失败.", e);
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * 发送静态资源的文件,如：图片
-	 * @param fromPos 发送地址
-	 * @param toPos 目的地址
-	 * @param subject 邮件主题
-	 * @param file 附件
-	 * @return 发送是否成功 
+	 * 
+	 * @param fromPos
+	 *            发送地址
+	 * @param toPos
+	 *            目的地址
+	 * @param subject
+	 *            邮件主题
+	 * @param file
+	 *            附件
+	 * @return 发送是否成功
 	 */
-	public boolean sendInlineMail(String fromPos, String toPos, String subject, FileSystemResource file)
-	{
+	public boolean sendInlineMail(String fromPos, String toPos, String subject,
+			FileSystemResource file) {
 		MimeMessage msg = mailSender.createMimeMessage();
-		try
-		{
-			//MimeMessageHelper构造器，如果要发送附件邮件，必须指定multipart参数为true
-			MimeMessageHelper helper  = new MimeMessageHelper(msg, true);
+		try {
+			// MimeMessageHelper构造器，如果要发送附件邮件，必须指定multipart参数为true
+			MimeMessageHelper helper = new MimeMessageHelper(msg, true);
 			helper.setFrom(fromPos);
 			helper.setTo(toPos);
 			helper.setSubject(subject);
-			helper.setText("<html><body>静态资源:<img src='cid:pic' /></body></html>", true);
+			helper.setText(
+					"<html><body>静态资源:<img src='cid:pic' /></body></html>",
+					true);
 			helper.addInline("pic", file);
 			mailSender.send(msg);
-		} 
-		catch (MessagingException e)
-		{
+		} catch (MessagingException e) {
 			logger.error("发送附件邮件失败.", e);
 			return false;
 		}
-		
+
 		return true;
 	}
-	
-	
+
 }
--- springboot-mail/src/main/java/mail/test/TestMail.java
+++ springboot-mail/src/main/java/mail/test/TestMail.java
@@ -7,6 +7,7 @@ import java.io.File;
 
 import mail.service.MailSendService;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,7 @@ public class TestMail
 	@Test
 	public void test()throws Exception
 	{
-		String fromPos = "xianlong.lu@sprixin.com";
+		String fromPos = "408030514@qq.com";
 		String toPos = "408030514@qq.com";
 		String subject = "今天的会议通知";
 		String text = "你好撒地方是个地方官 ";
@@ -37,10 +38,34 @@ public class TestMail
 		
 		FileSystemResource file = new FileSystemResource(new File("D:\\1.jpg"));
 		//2.测试发送附件邮件
-		mailSendService.sendAttachFileMail(fromPos, toPos, subject, text, file);
+		//mailSendService.sendAttachFileMail(fromPos, toPos, subject, text, file);
 		
 		//3.测试发送内置静态资源邮件
 		//mailSendService.sendInlineMail(fromPos, toPos, subject,  file);
+		
+		sendmails(fromPos, subject);
+		
+	}
+	private void sendmails(String fromPos, String subject){
+
+		StringBuffer stringBuffer  = new StringBuffer();
+		stringBuffer.append("<span>你好:</span><br/>");
+		String content = "根据已完成的换肤方案，经过查看筛选，决定取消换肤中的倒数第二个背景，保留其余的几种方案。需要取消的背景，参考下面图示。";
+		stringBuffer.append("<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + content + "</p>");
+		stringBuffer.append("<p>祝</p>");
+		stringBuffer.append("<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工作顺利！</p>");
+		
+		String [] toPoss = {"408030514222@qq.com", "40803051411@qq.com","408030514111@qq.com"};
+		String msg = "";
+		for (int i = 0; i < toPoss.length; i++) {
+			if(!mailSendService.sendHtmlMail(fromPos, toPoss[i], subject, stringBuffer.toString())){
+				msg += toPoss[i] + ",";
+			}
+		}
+		if (!StringUtils.isEmpty(msg)) {
+			msg = msg.substring(0, msg.length() - 1);
+		}
+		System.err.println(msg);
 	}
 	
 }
